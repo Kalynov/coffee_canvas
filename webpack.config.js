8160ceda9cf8a63e67477ca874eeeff72e4395f9
akalinov <a.kalynov@netris.ru>
2021-08-30 00:39:52 +0300

    add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.coffee',
+    output: {
+        filename: 'index.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.coffee$/,
+          loader: "coffee-loader",
+          options: {
+            bare: false,
+            transpile: {
+              presets: ["@babel/env"],
+            },
+          },
+        },
+      ],
+    },
+    devServer: {
+        port: 4200,
+        static: path.join(__dirname, 'public'),
+        hot: true,
+        allowedHosts: 'auto',
+        bonjour: true,
+      },
+  };
